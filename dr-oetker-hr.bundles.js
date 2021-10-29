c002f35fa10722d4024c9e4addaee518724463a5
Tihomir Jauk <tihomir.jauk@gmail.com>
2021-10-29 12:17:32 +0200

    Update script to v4.4 with external bundle definitions

---
+++ dr-oetker-hr.bundles.js
@@ -0,0 +1,506 @@
+var bundles = {
+    "170000528": [
+        {
+            "id": "3b5d153a6da14e33afa871b8a5e0c541",
+            "code": "170000514",
+            "name": "Kalup za voćni biskvit - Ø28 cm, crveno-sivi",
+            "quantity": 1
+        },
+        {
+            "id": "ce3eb712cc0145f5b0dc49b83c65409c",
+            "code": "170005525",
+            "name": "Original Backin 5+1",
+            "quantity": 1
+        },
+        {
+            "id": "f8b7dcc2b19541e593bf3febcd42b26e",
+            "code": "170004575",
+            "name": "Bourbon vanilin šećer x3",
+            "quantity": 1
+        },
+        {
+            "id": "eee84b97603f4dbcb1fef0afa6fb701d",
+            "code": "170002122",
+            "name": "Kremfix x3",
+            "quantity": 1
+        },
+        {
+            "id": "565b4481e299458d9981003803c7bd05",
+            "code": "170004878",
+            "name": "Preljev za torte jagoda",
+            "quantity": 2
+        },
+        {
+            "id": "ee8652f35a704c99ab920c6387c63b9d",
+            "code": "170002065",
+            "name": "Puding slatko vrhnje x3",
+            "quantity": 1
+        }
+    ],
+    "170000529": [
+        {
+            "id": "ae7024d5c11143cf817ee2af8d3d3841",
+            "code": "170000526",
+            "name": "Zeko kalup na zatvaranje, zlatni",
+            "quantity": 1
+        },
+        {
+            "id": "84c540e36cb040c99ef2055b149b706f",
+            "code": "101143500",
+            "name": "Finesse naribana korica limuna",
+            "quantity": 1
+        },
+        {
+            "id": "d06732846b8940009b60be18cfa42cef",
+            "code": "170003436",
+            "name": "Gustin 200g ",
+            "quantity": 1
+        },
+        {
+            "id": "18dee41bfd664c77b994cff325f4fb72",
+            "code": "170001190",
+            "name": "Prašak za pecivo",
+            "quantity": 2
+        },
+        {
+            "id": "1ada1bbaa2e74acb9f3e0ae6f8acc9d5",
+            "code": "170006722",
+            "name": "Vanilin šećer",
+            "quantity": 2
+        }
+    ],
+    "170000531": [
+        {
+            "id": "92824d7c3b5941a2bb521995d91b36a3",
+            "code": "170000505",
+            "name": "Limena doza \"Dodaci za kolače\"",
+            "quantity": 1
+        },
+        {
+            "id": "18dee41bfd664c77b994cff325f4fb72",
+            "code": "170001190",
+            "name": "Prašak za pecivo",
+            "quantity": 2
+        },
+        {
+            "id": "1ada1bbaa2e74acb9f3e0ae6f8acc9d5",
+            "code": "170006722",
+            "name": "Vanilin šećer",
+            "quantity": 3
+        },
+        {
+            "id": "dc68584e5a9c4e9c84e1eb25a050fc9d",
+            "code": "170006856",
+            "name": "Limun šećer x3",
+            "quantity": 1
+        },
+        {
+            "id": "003485792b284d40bac4c897766d329b",
+            "code": "170006858",
+            "name": "Rum šećer x3",
+            "quantity": 1
+        },
+        {
+            "id": "f334c51fe38b482f83c7381e80e75b81",
+            "code": "170011102",
+            "name": "Kvasac instant",
+            "quantity": 2
+        },
+        {
+            "id": "eee84b97603f4dbcb1fef0afa6fb701d",
+            "code": "170002122",
+            "name": "Kremfix x3",
+            "quantity": 1
+        },
+        {
+            "id": "0e33703f5295474fb8a7caed78c1caff",
+            "code": "170001655",
+            "name": "Preljev, svijetli x3",
+            "quantity": 1
+        },
+        {
+            "id": "4be092aa5e134d52915065447b824283",
+            "code": "170001741",
+            "name": "Želatina u listićima, svijetla",
+            "quantity": 1
+        },
+        {
+            "id": "79e74cc94b7c4305bdda54fc304271ca",
+            "code": "170001232",
+            "name": "Soda bikarbona",
+            "quantity": 2
+        }
+    ],
+    "170000532": [
+        {
+            "id": "498a473852164380b5228f3cd7a20e4d",
+            "code": "170000507",
+            "name": "Vintage doza \"Prijateljice\"",
+            "quantity": 1
+        },
+        {
+            "id": "8724105180a24f8c9c067398779d0529",
+            "code": "170005140",
+            "name": "Pasta od bourbon vanilije",
+            "quantity": 1
+        },
+        {
+            "id": "7199edebae4841be81830310489d288b",
+            "code": "170005483",
+            "name": "Ukrasni cvjetići",
+            "quantity": 1
+        },
+        {
+            "id": "629e484ebe8740158b5a018a039f9c5b",
+            "code": "101531300",
+            "name": "Čokoladno pismo",
+            "quantity": 1
+        },
+        {
+            "id": "a0e0133b5710476abe4a2a2d3304c8a0",
+            "code": "170015514",
+            "name": "Dekor Sweet Rose",
+            "quantity": 1
+        }
+    ],
+    "170000530": [
+        {
+            "id": "4817d51b8b7d49449b4d36ee830c4982",
+            "code": "170000527",
+            "name": "Retro okrugli kalup Ø20 cm",
+            "quantity": 1
+        },
+        {
+            "id": "ee7e6a9389fd478d9be89a3d431b8b60",
+            "code": "170006849",
+            "name": "Čokolada cake mix",
+            "quantity": 1
+        },
+        {
+            "id": "9cb0e6c91d894e62b9dfa02a5e62e479",
+            "code": "170006850",
+            "name": "Vanilija cake mix",
+            "quantity": 1
+        },
+        {
+            "id": "a0e0133b5710476abe4a2a2d3304c8a0",
+            "code": "170015514",
+            "name": "Dekor Sweet Rose",
+            "quantity": 1
+        },
+        {
+            "id": "f822c06e5d09442bb74dc5d5e9e4c2ce",
+            "code": "170015513",
+            "name": "Dekor Creation Blue",
+            "quantity": 1
+        },
+        {
+            "id": "22d4caec033f4b60b2216812b6ef54d7",
+            "code": "170001226",
+            "name": "Krema za torte čokolada",
+            "quantity": 1
+        }
+    ],
+    "170000540": [
+        {
+            "id": "fa74af0c16d742209ecaecf130382b4b",
+            "code": "170000533",
+            "name": "Retro roza kuhinjska rukavica ",
+            "quantity": 1
+        },
+        {
+            "id": "d2a3e2b974bd41008b92d12a8f35211f",
+            "code": "170005396",
+            "name": "Mini Muffins",
+            "quantity": 1
+        },
+        {
+            "id": "b761a4a4f70545f0b2868a2d4a5e6c40",
+            "code": "101526000",
+            "name": "Svjećice Happy Birthday",
+            "quantity": 1
+        }
+    ],
+    "170000543": [
+        {
+            "id": "483b86156b154876852630d72ab9c050",
+            "code": "170002560",
+            "name": "Riža na mlijeku klasik",
+            "quantity": 1
+        },
+        {
+            "id": "918c696460324e28b4bb66cb43a4f9fb",
+            "code": "170002561",
+            "name": "Riža na mlijeku vanilija",
+            "quantity": 1
+        },
+        {
+            "id": "4cda9c67a8e64b9b81407ebf176bb4a1",
+            "code": "170006534",
+            "name": "Američke palačinke s proteinima",
+            "quantity": 1
+        },
+        {
+            "id": "1dc544f5c0bb4257b150c9ff99697fb8",
+            "code": "170006612",
+            "name": "Waffles",
+            "quantity": 1
+        }
+    ],
+    "170000538": [
+        {
+            "id": "a0fe4f12e2c84e849afb1185a4f5ed5e",
+            "code": "170000537",
+            "name": "Široki lijevak za staklenke",
+            "quantity": 1
+        },
+        {
+            "id": "aae922dd2f99452d86702164d5941b92",
+            "code": "170000535",
+            "name": "Samoljepljive etikete za staklenke, 16 kom",
+            "quantity": 1
+        },
+        {
+            "id": "684ef51b325b4cf2ada1236a72a3d9a4",
+            "code": "170011234",
+            "name": "Džemfix Extra 2:1",
+            "quantity": 2
+        },
+        {
+            "id": "1f8c1d2115a24e578524e1755538c772",
+            "code": "170011556",
+            "name": "Džemfix Classic 1:1",
+            "quantity": 2
+        },
+        {
+            "id": "8d063646463e41a582f99f411b4ec824",
+            "code": "170015082",
+            "name": "Džemfix Super 3:1",
+            "quantity": 2
+        },
+        {
+            "id": "e016e02d45324ee298164062f98cfeb3",
+            "code": "170001233",
+            "name": "Limunska kiselina",
+            "quantity": 2
+        },
+        {
+            "id": "bb33d29891b74e1db9d3ed2277b697b2",
+            "code": "170006785",
+            "name": "Štapić vanilije",
+            "quantity": 1
+        }
+    ],
+    "170000539": [
+        {
+            "id": "a35ed0e24b0441ef878a741d9ab3567e",
+            "code": "170000544",
+            "name": "Knjiga \"Kolači s pudingom\"",
+            "quantity": 1
+        },
+        {
+            "id": "d0675e8ff97541d596c6b047493a25e7",
+            "code": "170021236",
+            "name": "Puding vanilija",
+            "quantity": 3
+        },
+        {
+            "id": "13c68c0344a54f3da9241c4ebb58e3c2",
+            "code": "170004549",
+            "name": "Puding komadići čokolade",
+            "quantity": 2
+        },
+        {
+            "id": "7689d1cdbba0428d8f7552c3747d6dea",
+            "code": "170006061",
+            "name": "Special Puding za kremšnite",
+            "quantity": 1
+        },
+        {
+            "id": "6043879c47af4c27802e8acefe7f53ee",
+            "code": "170006064",
+            "name": "Special Puding za mađaricu ",
+            "quantity": 1
+        }
+    ],
+    "170000541": [
+        {
+            "id": "684ef51b325b4cf2ada1236a72a3d9a4",
+            "code": "170011234",
+            "name": "Džemfix Extra 2:1",
+            "quantity": 4
+        }
+    ],
+    "170000547": [
+        {
+            "id": "4c6f36c631e54dd1918cd3b51b7000ea",
+            "code": "170000508",
+            "name": "Vintage doza \"Kvaliteta je najbolji recept\"",
+            "quantity": 1
+        },
+        {
+            "id": "ce3eb712cc0145f5b0dc49b83c65409c",
+            "code": "170005525",
+            "name": "Original Backin 5+1",
+            "quantity": 1
+        },
+        {
+            "id": "f8b7dcc2b19541e593bf3febcd42b26e",
+            "code": "170004575",
+            "name": "Bourbon vanilin šećer x3",
+            "quantity": 1
+        },
+        {
+            "id": "e59faeea76324bab80bb773716ab0fa0",
+            "code": "170006795",
+            "name": "Instant Želatina Fix",
+            "quantity": 1
+        },
+        {
+            "id": "5a36ab01cfd54e148262e3ce2ff90ac0",
+            "code": "170011516",
+            "name": "Crni kakao",
+            "quantity": 1
+        },
+        {
+            "id": "84c540e36cb040c99ef2055b149b706f",
+            "code": "101143500",
+            "name": "Finesse naribana korica limuna",
+            "quantity": 1
+        }
+    ],
+    "170000548": [
+        {
+            "id": "3b5d153a6da14e33afa871b8a5e0c541",
+            "code": "170000514",
+            "name": "Kalup za voćni biskvit - Ø28 cm, crveno-sivi",
+            "quantity": 1
+        },
+        {
+            "id": "ce3eb712cc0145f5b0dc49b83c65409c",
+            "code": "170005525",
+            "name": "Original Backin 5+1",
+            "quantity": 1
+        },
+        {
+            "id": "f8b7dcc2b19541e593bf3febcd42b26e",
+            "code": "170004575",
+            "name": "Bourbon vanilin šećer x3",
+            "quantity": 1
+        },
+        {
+            "id": "eee84b97603f4dbcb1fef0afa6fb701d",
+            "code": "170002122",
+            "name": "Kremfix x3",
+            "quantity": 1
+        },
+        {
+            "id": "d0675e8ff97541d596c6b047493a25e7",
+            "code": "170021236",
+            "name": "Puding vanilija",
+            "quantity": 2
+        }
+    ],
+    "170000582": [
+        {
+            "id": "b768b6edb68e4283839af0262aef1e72",
+            "code": "170000515",
+            "name": "Kalup za 12 muffina - 26,5x38,5x2 cm",
+            "quantity": 1
+        },
+        {
+            "id": "bdad7921d60f4f3ab79cbe58bef7568a",
+            "code": "170003781",
+            "name": "Čokoladni Muffins",
+            "quantity": 1
+        },
+        {
+            "id": "130bfae453b84a0181a34312a2aa045b",
+            "code": "170016535",
+            "name": "Mramorni muffins",
+            "quantity": 1
+        },
+        {
+            "id": "55da4d29e3ee4d0c9cea590bd8ac3c17",
+            "code": "170015129",
+            "name": "Šareno pismo",
+            "quantity": 1
+        },
+        {
+            "id": "1b8049a488534cfca4d6c7d90ab776fb",
+            "code": "170015517",
+            "name": "Dekor mix zvjezdice",
+            "quantity": 1
+        }
+    ],
+    "170000583": [
+        {
+            "id": "960432ed82fb445597eccd5af5e49b2a",
+            "code": "170006555",
+            "name": "Voćni muffini - banana",
+            "quantity": 1
+        },
+        {
+            "id": "839c9c3a7e9647d7924835671bd30851",
+            "code": "170003780",
+            "name": "Muffins",
+            "quantity": 1
+        },
+        {
+            "id": "3b3cd59f965a4888889c44dc5138229d",
+            "code": "170000570",
+            "name": "Papirnate košarice za muffine SREĆA, 50 kom",
+            "quantity": 1
+        },
+        {
+            "id": "942a4d8d42164a5a9c760f2e7bb6e7de",
+            "code": "170005556",
+            "name": "Mirror glaze šećerna glazura",
+            "quantity": 1
+        },
+        {
+            "id": "e6f074deba2d446a8c9c5b5a446106f0",
+            "code": "170025516",
+            "name": "Dekor mix perle soft",
+            "quantity": 1
+        },
+        {
+            "id": "49f26a5e88ed43b1b9ac4665e802c9cf",
+            "code": "170000556",
+            "name": "Posuda za spremanje muffina Candy",
+            "quantity": 1
+        }
+    ],
+    "170000585": [
+        {
+            "id": "e88252e875e54f8bb12a34ce2a6e4192",
+            "code": "170006842",
+            "name": "Super kaša sa šumskim voćem i chia sjemenkama",
+            "quantity": 5
+        }
+    ],
+    "170000586": [
+        {
+            "id": "0e84ce8a7dc447808b3247b9fca3a1c0",
+            "code": "170006446",
+            "name": "Vitalis hrskavi muesli s medom",
+            "quantity": 4
+        }
+    ],
+    "170000587": [
+        {
+            "id": "b3ac1b5a2ede41f68fdfc4ea1d4c970b",
+            "code": "101687600",
+            "name": "Vitalis SuperMuesli bez dodanog šećera",
+            "quantity": 4
+        }
+    ],
+    "170000596": [
+        {
+            "id": "433e4109916e4c2b8e4318306cc2f7da",
+            "code": "170000595",
+            "name": "Kalendar s receptima 2022.",
+            "quantity": 4
+        }
+    ]
+};
